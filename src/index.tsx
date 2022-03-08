b13f7d13fc233d93f04353cc75f9fabfe961d132
joao.chagas <joao.chagas@webjump.com.br>
2022-03-08 14:55:37 -0300

    add interceptors to request errors and remove unnecessary lines, beyond adjust file format

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'mobx-react-lite/batchingForReactDom'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
@@ -10,10 +10,43 @@ import ContextProviderComposer from './providers/contextProviderComposer'
 import Router from './routes'
 import * as serviceWorker from './serviceWorker'
 import { GlobalStyles } from './styles/global'
-import axios from 'axios'
+import { api as CinejumpAPI } from 'apis/cinejump'
+import { RenderAToast } from 'helpers/toast'
 
 const App: React.FC = () => {
-  axios.defaults.baseURL = 'http://localhost:8888'
+  const [errorInterceptor, setErrorInterceptor] = useState<number | undefined>(undefined)
+
+  const addErrorInterceptor = () => {
+    const errorInterceptor = CinejumpAPI.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (error) => {
+        let { data, status } = error.response
+        let message = data.message || data.error
+        RenderAToast('error', message)
+        if (status === 401 && window.location.pathname !== '/user') {
+          window.location.replace('/user')
+          localStorage.clear()
+        }
+        return Promise.reject(error)
+      }
+    )
+    setErrorInterceptor(errorInterceptor)
+  }
+
+  const removeErrorInterceptor = () => {
+    CinejumpAPI.interceptors.request.eject(errorInterceptor || 0)
+    setErrorInterceptor(undefined)
+  }
+
+  useEffect(() => {
+    addErrorInterceptor()
+    return () => {
+      removeErrorInterceptor()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <ContextProviderComposer contextProviders={Providers}>
